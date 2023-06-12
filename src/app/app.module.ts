ed3a96a517851724a82086e8776a3fef55413f0d
Ana Carolina <carolinamatosinhos98@gmail.com>
2023-06-11 23:45:58 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutComponent } from './layout/layout.component';
+import { NavbarComponent } from './_layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
